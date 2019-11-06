4bd0aa6f435a7d8714805b9c8f8b558b28ab8b6a
raxanob <rayanexavier161@gmail.com>
2019-11-06 17:52:12 -0300

    nao construir os mortos, os vivinos no eixo diferente, algumas coisas sobre camera e luz, assim como performace

--- GameLife/GameViewController.swift
+++ GameLife/GameViewController.swift
@@ -1,49 +1,40 @@
-//
-//  GameViewController.swift
-//  GameLife
-//
-//  Created by Rayane Xavier on 31/10/19.
-//  Copyright © 2019 Rayane Xavier. All rights reserved.
-//
+    //
+    //  GameViewController.swift
+    //  GameLife
+    //
+    //  Created by Rayane Xavier on 31/10/19.
+    //  Copyright © 2019 Rayane Xavier. All rights reserved.
+    //
 
-import UIKit
-import QuartzCore
-import SceneKit
+    import UIKit
+    import QuartzCore
+    import SceneKit
 
-class GameViewController: UIViewController {
+    class GameViewController: UIViewController {
 
-    let scene = GameScene()
-    
-    override func viewDidLoad() {
-
-    // retrieve the SCNView
-    let scnView = self.view as! SCNView
-    
-    // set the scene to the view
-    scnView.scene = scene
-    scnView.pointOfView?.position = SCNVector3Make(0, 0, 0)
-
-    // allows the user to manipulate the camera
-    scnView.allowsCameraControl = true
-
-    // show statistics such as fps and timing information
-    scnView.showsStatistics = true
-
-    // configure the view
-     scnView.backgroundColor = UIColor.black
+        let scene = GameScene()
         
-    // create and add a camera to the scene
-    let cameraNode = SCNNode()
-    cameraNode.camera = SCNCamera()
-    scene.rootNode.addChildNode(cameraNode)
-            
-    // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: -30, z: 10)
-        cameraNode.look(at: SCNVector3 (0, 0, 0))
-    }
-    
+        override func viewDidLoad() {
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        scene.touchedScreen()
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        scnView.delegate = scene
+        scnView.loops = true
+        scnView.isPlaying = true
+            
+        // set the scene to the view
+        scnView.scene = scene
+        scnView.pointOfView = scene.cameraNode
+
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = false
+
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+
+        // configure the view
+         scnView.backgroundColor = UIColor.black
+
+        }
+        
     }
-}
--- GameScene.swift
+++ GameScene.swift
@@ -9,7 +9,7 @@
 
 import SceneKit
 
-public class GameScene: SCNScene {
+public class GameScene: SCNScene, SCNSceneRendererDelegate {
     
     let deadColor = UIColor.systemGray5
     let aliveColor = UIColor.systemRed
@@ -17,17 +17,22 @@ public class GameScene: SCNScene {
     let rules = RulesGame()
     var nodes: [SCNNode] = []
     var z: Float = 0
+    let cameraNode = SCNNode()
+    let boxGeometry = SCNBox(width: 0.6, height: 0.6, length: 0.6, chamferRadius: 0.05)
     
     override public init() {
         super.init()
+        setupScene()
+        self.boxGeometry.firstMaterial?.diffuse.contents = aliveColor
         
-        grid.matrix[1][3].isAlive = 1
-        grid.matrix[2][2].isAlive = 1
-        grid.matrix[2][3].isAlive = 1
         grid.matrix[3][3].isAlive = 1
-        grid.matrix[3][4].isAlive = 1
+        grid.matrix[4][2].isAlive = 1
+        grid.matrix[4][3].isAlive = 1
+        grid.matrix[5][4].isAlive = 1
 
-        drowGrid()
+
+
+        drawGrid()
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -35,9 +40,9 @@ public class GameScene: SCNScene {
         
     }
     
-    func drowGrid() {
+    func drawGrid() {
         
-        let offset: Int = 3
+        let offset: Int = 4
         let rows = grid.matrix.count
         let primeiraLinha = grid.matrix[0]
         let cols = primeiraLinha.count
@@ -46,31 +51,59 @@ public class GameScene: SCNScene {
             var str: String = ""
             for j in 0..<cols {
                 str.append("\(grid.matrix[i][j].isAlive)")
-                let boxGeometry = SCNBox(width: 0.6, height: 0.6, length: 0.6, chamferRadius: 0.05)
-                let boxCopy = SCNNode(geometry: boxGeometry)
 
                 if grid.matrix[i][j].isAlive == 1 {
-                    boxCopy.geometry?.firstMaterial?.diffuse.contents = aliveColor
-                } else {
-                    boxCopy.geometry?.firstMaterial?.diffuse.contents = deadColor
+                    let boxCopy = SCNNode(geometry: boxGeometry)
+                    boxCopy.position.x = Float(i - offset)
+                    boxCopy.position.y = Float(j - offset)
+                    boxCopy.position.z = z
+                    nodes.append(boxCopy)
+                    self.rootNode.addChildNode(boxCopy)
                 }
-
-                boxCopy.position.x = Float(i - offset)
-                boxCopy.position.y = Float(j - offset)
-                boxCopy.position.z = z
-                nodes.append(boxCopy)
-                self.rootNode.addChildNode(boxCopy)
-
             }
             print(str)
         }
     }
     
-    func touchedScreen() {
-        z = z + 1
-        nodes = []
+    func reload() {
+        z += 1
+        cameraNode.position.z += 1
         grid = rules.step(grid: grid)
-        drowGrid()
+        drawGrid()
+    }
+    
+    let interval: TimeInterval = 0.1
+    var nextTime: TimeInterval = 0
+    public func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        if nextTime < time {
+        reload()
+        nextTime = time + interval
+        }
+    }
+    
+    func setupScene(){
+        // create and add a camera to the scene
+        
+        cameraNode.camera = SCNCamera()
+                
+        // place the camera
+        cameraNode.position = SCNVector3(x: 0, y: -40, z: 10)
+        cameraNode.look(at: SCNVector3 (0, 0, 0))
+        self.rootNode.addChildNode(cameraNode)
+            
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 20, z: 20)
+        self.rootNode.addChildNode(lightNode)
+        
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = .ambient
+        ambientLightNode.light!.color = UIColor.darkGray
+        self.rootNode.addChildNode(ambientLightNode)
     }
     
 }
